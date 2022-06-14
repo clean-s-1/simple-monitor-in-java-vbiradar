eff9a6c4e1ab4b624a1fb6b5b14807b522579ed4
Vikhyat S Biradar <vikhyat.biradar@siemens.com>
2022-06-14 17:25:52 +0530

    separated the logic and tested

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,18 +2,38 @@ package vitals;
 
 public class Main {
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
+
+        if(checkIsbatteryOkByTemperature(temperature) && checkIsbatteryOkBySoc(soc) && checkIsbatteryOkByChargeRate(chargeRate)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static boolean checkIsbatteryOkByTemperature(float temperature) {
+        if (temperature < 0 || temperature > 45) {
             System.out.println("Temperature is out of range!");
             return false;
-        } else if(soc < 20 || soc > 80) {
+        }
+        return true;
+    }
+
+    static boolean checkIsbatteryOkBySoc(float soc) {
+        if (soc < 20 || soc > 80) {
             System.out.println("State of Charge is out of range!");
             return false;
-        } else if(chargeRate > 0.8) {
+        }
+        return true;
+    }
+
+    static boolean checkIsbatteryOkByChargeRate(float chargeRate) {
+        if (chargeRate > 0.8) {
             System.out.println("Charge Rate is out of range!");
             return false;
         }
         return true;
     }
+
 
     public static void main(String[] args) {
         assert(batteryIsOk(25, 70, 0.7f) == true);
