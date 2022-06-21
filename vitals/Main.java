4da53f03971d46f327732ed5d1709b2d626efa23
Vikhyat S Biradar <vikhyat.biradar@siemens.com>
2022-06-21 10:17:14 +0530

    Added test cases

--- vitals/Main.java
+++ vitals/Main.java
@@ -12,6 +12,7 @@ public class Main {
         assert (batteryIsOk(25, 70, 0.7f, BatteryHealthFactor.valueOf("CHARGE_STATE")) == true);
         assert (batteryIsOk(25, 85, 0.0f, BatteryHealthFactor.valueOf("CHARGE_STATE")) == false);
         assert (batteryIsOk(25, 70, 0.9f, BatteryHealthFactor.valueOf("CHARGE_RATE")) == false);
+        assert (batteryIsOk(30, 40, 0.7f, BatteryHealthFactor.valueOf("CHARGE_RATE")) == true);
         System.out.println("Battery Condition tested");
     }
 }
